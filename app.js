9290436b78eb7e457096e7f1e3f53c34675c8f13
stefano-b <bartoletti.stefano@gmail.com>
2020-03-18 18:43:19 +0100

    v-on directive, listen to events

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ new Vue({
         title: 'Hello World!',
         link: 'https://www.google.com',
         fullLink: '<a href="https://www.google.com" target="_blank">Google</a>',
+        counter: 0,
     },
     methods: {
       sayHello: function() {
         this.title = 'Hello!'
         return  'The title is ' + this.title;
+      },
+      increase: function() {
+        this.counter++;
       }
     }
 });
