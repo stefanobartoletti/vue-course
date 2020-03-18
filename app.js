91f69060f4817d5d30872a8961f361ccfb97df58
stefano-b <bartoletti.stefano@gmail.com>
2020-03-18 17:52:15 +0100

    v-html directive

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        link: 'https://www.google.com'
+        link: 'https://www.google.com',
+        fullLink: '<a href="https://www.google.com" target="_blank">Google</a>',
     },
     methods: {
       sayHello: function() {
