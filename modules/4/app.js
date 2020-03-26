8aa790b847960fbd63ed26b6a1831803f456718d
stefano-b <bartoletti.stefano@gmail.com>
2020-03-26 20:07:51 +0100

    action log setup

--- modules/4/app.js
+++ modules/4/app.js
@@ -6,6 +6,7 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: [],
     },
 
     computed: { 
@@ -25,7 +26,12 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage,
+            })
             if (this.checkWin()) { return; }
             this.monsterAttack();
         },
@@ -47,8 +53,13 @@ new Vue({
         },
 
         monsterAttack: function() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage,
+            });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
