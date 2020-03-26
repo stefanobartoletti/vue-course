1a8ab28c7ecff88001def08005b4afe53ee318bf
stefano-b <bartoletti.stefano@gmail.com>
2020-03-26 19:21:21 +0100

    implement special attack

--- modules/4/app.js
+++ modules/4/app.js
@@ -25,22 +25,25 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function(){
-
             this.monsterHealth -= this.calculateDamage(3, 10);
             if (this.checkWin()) { return; }
-
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
-
+            this.monsterAttack();
         },
         specialAttack: function(){
-
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) { return; }
+            this.monsterAttack();
         },
         heal: function(){
 
         },
         giveUp: function(){
 
+        },
+
+        monsterAttack: function() {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
