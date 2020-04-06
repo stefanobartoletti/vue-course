cce364ee5d1c80ab57635671ce0fa950209945df
stefano-b <bartoletti.stefano@gmail.com>
2020-04-06 18:55:00 +0200

    vue instance lifecycle methods

---
+++ modules/5/a/app.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#app',
+  data: { 
+    title: 'Vue instance',
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()')
+  },
+  created: function() {
+    console.log('created()')
+  },
+  beforeMount: function() {
+    console.log('beforeMount()')
+  },
+  mounted: function() {
+    console.log('mounted()')
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()')
+  },
+  updated: function() {
+    console.log('updated()')
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()')
+  },
+  destroyed: function() {
+    console.log('destroyed()')
+  },
+  methods: {
+    destroy: function() {
+      this.$destroy();
+    }
+  }
+});
