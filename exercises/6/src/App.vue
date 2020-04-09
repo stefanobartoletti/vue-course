bf8df5c0a06dd23cec0ab52ac5bdb3af81867648
stefano-b <bartoletti.stefano@gmail.com>
2020-04-09 17:23:26 +0200

    my solution

--- exercises/6/src/App.vue
+++ exercises/6/src/App.vue
@@ -1,39 +1,15 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <dashboard-header></dashboard-header>
         <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
+        <dashboard-content></dashboard-content>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <dashboard-footer></dashboard-footer>
     </div>
 </template>
 
 <script>
+
 </script>
 
 <style>
---
+++ exercises/6/src/ServerList.vue
@@ -0,0 +1,17 @@
+<template>   
+    <ul class="list-group">
+        <li class="list-group-item" v-for="index in 5">
+            Server #{{ index }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
