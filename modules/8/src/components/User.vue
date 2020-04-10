7f401efc608106a32fd8995f495e2bfb7c72d557
stefano-b <bartoletti.stefano@gmail.com>
2020-04-10 17:04:35 +0200

    'props' pass data from parent to child components

--- modules/8/src/components/User.vue
+++ modules/8/src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :name="name"></app-user-detail> <!-- v-bind to pass data to child components -->
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +20,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function() {
+            return {
+                name: 'Bilbo',
+            };
+        },
+        methods: {
+            changeName() {
+                this.name = 'Frodo';
+            },
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
