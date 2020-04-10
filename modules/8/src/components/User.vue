9ca1c9f875799b44eb7913e0a90d4e16b3ca1f21
stefano-b <bartoletti.stefano@gmail.com>
2020-04-10 18:04:44 +0200

    custom events to pass data from child to parent

--- modules/8/src/components/User.vue
+++ modules/8/src/components/User.vue
@@ -3,10 +3,11 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my name</button>
+        <p>Name is {{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name"></app-user-detail> <!-- v-bind to pass data to child components -->
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail> <!-- v-bind to pass data to child components // v-on to listen to custom events passed by child elements -->
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
--- modules/8/src/components/UserDetail.vue
+++ modules/8/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ reverseName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
@@ -17,6 +18,10 @@
         methods : {
             reverseName() {
                 return this.myName.split('').reverse().join(''); // props can be used like data in functions
+            },
+            resetName() {
+                this.myName = 'Bilbo';
+                this.$emit('nameWasReset', this.myName); // emits a custom event to pass the new value to the parent 
             }
         }
     }
