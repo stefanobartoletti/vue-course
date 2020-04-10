e75059884650daed7baacc96d2748efd9c3afa4e
stefano-b <bartoletti.stefano@gmail.com>
2020-04-10 19:10:30 +0200

    centralize code in the event bus

--- modules/8/src/components/UserEdit.vue
+++ modules/8/src/components/UserEdit.vue
@@ -16,7 +16,8 @@
             editAge() {
                 this.userAge = 30;
                 // this.$emit('ageWasEdited', this.userAge)
-                eventBus.$emit('ageWasEdited', this.userAge)
+                // eventBus.$emit('ageWasEdited', this.userAge)
+                eventBus.changeAge(this.userAge);
             }
         }
     }
--- modules/8/src/main.js
+++ modules/8/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue();
+export const eventBus = new Vue({
+  methods: {
+    changeAge(age) {
+      this.$emit('ageWasEdited', age)
+    }
+  }
+});
 
 new Vue({
   el: '#app',
