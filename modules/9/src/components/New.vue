e3f8ab2d655a15c21a595795cad1d5d6f5eff94d
stefano-b <bartoletti.stefano@gmail.com>
2020-04-13 20:05:27 +0200

    dynamic components destroyed when switched

--- modules/9/src/components/New.vue
+++ modules/9/src/components/New.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{counter}}</p>
     </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        data: function() {
+            return {
+                counter: 0,
+            }
+        },
+        destroyed() {
+            console.log('"New" was destroyed')
+        }
+    }
 </script>
 
 <style scoped>
