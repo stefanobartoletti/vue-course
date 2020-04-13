ef9f7b0e2219d7caf22dd0d43e74aa8ecb6dd4f9
stefano-b <bartoletti.stefano@gmail.com>
2020-04-13 18:16:44 +0200

    exercise

---
+++ exercises/7/src/components/Server/Server.vue
@@ -0,0 +1,27 @@
+<template>
+    <li 
+        class="list-group-item"
+        @click="serverSelected"
+    >
+        Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+    import { serverBus } from '../../main';
+
+    export default {
+        props: ['server'],
+        methods: {
+            serverSelected() {
+                serverBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    li {
+        cursor: pointer;
+    }
+</style>
--- exercises/7/src/components/Server/ServerDetails.vue
+++ exercises/7/src/components/Server/ServerDetails.vue
@@ -1,11 +1,33 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a server</p>
+        <p v-else>Server #{{ server.id }} selected. Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Reset Status</button>
     </div>
 
 </template>
 
 <script>
+    import { serverBus } from '../../main';
+
+    export default {
+        data: function() {
+            return {
+                server: null,
+            }
+        },
+        methods: {
+            resetStatus() {
+                this.server.status = "Normal"
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server;
+            });
+        }
+    }
 </script>
 
 <style>
