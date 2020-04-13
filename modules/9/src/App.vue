6dd0a29c9c93e87a400a40c2cdf944c718d76b26
stefano-b <bartoletti.stefano@gmail.com>
2020-04-13 19:04:42 +0200

    pass content by using "slot"

--- modules/9/src/App.vue
+++ modules/9/src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote></app-quote>
+                <app-quote>
+                    <h2>The Quote</h2>
+                    <p>A wonderful quote!</p>
+                </app-quote>
             </div>
         </div>
     </div>
--- modules/9/src/components/Quote.vue
+++ modules/9/src/components/Quote.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <p>This is a quote!</p>
+        <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: []
 }
 </script>
 
