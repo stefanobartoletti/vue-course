f12fbf6474b8f543e21351dcfa1d6f1afa90282d
stefano-b <bartoletti.stefano@gmail.com>
2020-04-13 19:59:05 +0200

    dynamic compoenents

--- modules/9/src/App.vue
+++ modules/9/src/App.vue
@@ -2,10 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <component :is="selectedComponent">
+                </component>
+                
+                <!-- 
                 <app-quote>
                     <h2 slot="title">{{ quoteTitle}}</h2>
                     <p slot="content">A wonderful quote!</p>
                 </app-quote>
+                -->
             </div>
         </div>
     </div>
@@ -14,15 +24,20 @@
 <script>
 
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function() {
             return {
                 quoteTitle: 'The Title',
+                selectedComponent: 'appQuote'
             }
         },
         components: {
             AppQuote: Quote,
+            AppAuthor: Author,
+            AppNew: New,
         }
         
     }
---
+++ modules/9/src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
