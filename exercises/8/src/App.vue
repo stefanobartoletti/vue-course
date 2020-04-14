7e2359272e6fe4b2aa6fc1dec5b2e69b792a9b23
stefano-b <bartoletti.stefano@gmail.com>
2020-04-14 16:51:16 +0200

    my solution

--- exercises/8/src/App.vue
+++ exercises/8/src/App.vue
@@ -3,13 +3,17 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="selectedTemplate = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedTemplate = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedTemplate = 'appRed'">Load Red Template</button>
+                <span class="btn btn-transparent normalcursor">Selected Template: {{selectedTemplate}}</span>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <keep-alive>
+                    <component :is="selectedTemplate">
+                        <h2 slot="title">This is {{ selectedTemplate }}</h2>
+                        <p>A wonderful quote!</p>
+                    </component>
+                </keep-alive>
             </div>
         </div>
     </div>
@@ -21,6 +25,11 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function() {
+            return {
+                selectedTemplate: 'appBlue'
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
@@ -29,5 +38,10 @@
     }
 </script>
 
-<style>
+<style scoped>
+
+    .normalcursor {
+        cursor: default !important;
+    }
+
 </style>
--- exercises/8/src/components/Blue.vue
+++ exercises/8/src/components/Blue.vue
@@ -1,5 +1,7 @@
 <template>
-    <div></div>
+    <div>
+        <slot name="title"></slot>
+    </div>
 </template>
 
 <script>
