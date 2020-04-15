2c1b3df345528b6d4f01f70276cad4698e9e30dc
stefano-b <bartoletti.stefano@gmail.com>
2020-04-15 20:30:56 +0200

    progress bar

--- modules/10/src/App.vue
+++ modules/10/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuoteAdded"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -15,6 +16,7 @@
 <script>
     import QuoteGrid  from './components/QuoteGrid.vue';
     import NewQuote  from './components/NewQuote.vue';
+    import Header  from './components/Header.vue';
 
     export default {
         data: function() {
@@ -36,6 +38,7 @@
         components: {
             appQuoteGrid: QuoteGrid,
             appNewQuote: NewQuote,
+            appHeader: Header,
         }
         
     }
