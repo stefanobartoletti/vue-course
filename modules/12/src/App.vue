b4d4f6dbacccdfe02f04da7754d2f1c1307de294
stefano-b <bartoletti.stefano@gmail.com>
2020-04-21 17:37:48 +0200

    simple custom directive

--- modules/12/src/App.vue
+++ modules/12/src/App.vue
@@ -2,8 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
+                <h1>Built-in Directives</h1>
+                <p v-text="'Some text'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
 
+            </div>
+        </div>
+        <hr>        
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Color this</p>
             </div>
         </div>
     </div>
--- modules/12/src/main.js
+++ modules/12/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = "green";
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
