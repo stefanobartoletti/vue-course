bdb4afa0e64743375799e86e62e9a88fc28fb839
stefano-b <bartoletti.stefano@gmail.com>
2020-04-21 18:24:18 +0200

    pass modifiers to directives

--- modules/12/src/App.vue
+++ modules/12/src/App.vue
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight:background="'red'">Color this</p>
+                <p v-highlight:background.delayed="'red'">Color this</p>
             </div>
         </div>
     </div>
--- modules/12/src/main.js
+++ modules/12/src/main.js
@@ -5,11 +5,17 @@ Vue.directive('highlight', {
   bind(el, binding, vnode) {
     // el.style.backgroundColor = "green";
     //el.style.backgroundColor = binding.value;
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    var delay = 0;
+    if (binding.modifiers['delayed']){
+      delay = 3000;
     };
+    setTimeout( () => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
