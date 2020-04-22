9122f0b1e5c14a51c83b2ed75f8cebde7fb94ea7
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 18:07:48 +0200

    mixins for duplicate code

--- modules/13/src/App.vue
+++ modules/13/src/App.vue
@@ -9,12 +9,16 @@
                 <ul>
                     <li v-for="fruit in filteredFruits">{{fruit}}</li>
                 </ul>
+                <hr>
+                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import List from './List.vue';
+
     export default {
         data() {
             return {
@@ -34,6 +38,9 @@
                     return element.match(this.filterText);
                 });
             }
+        },
+        components: {
+            appList: List,
         }
     }
 </script>
---
+++ modules/13/src/List.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{fruit}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'Hello there!',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: '',
+            }
+        },
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter( (element) => {
+                    return element.match(this.filterText);
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
