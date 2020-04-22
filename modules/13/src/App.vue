4ddc8ea1bb71b1e11fbc42d6c71ade26cad20081
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 17:33:26 +0200

    global filters and using multiple ones

--- modules/13/src/App.vue
+++ modules/13/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase /* values are filtered with a pipe */ }}</p>
+                <p>{{ text | toUppercase | toLowercase }}</p> <!-- values are filtered with a pipe -->
             </div>
         </div>
     </div>
--- modules/13/src/main.js
+++ modules/13/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('toLowercase', function(value) {
+    return value.toLowerCase();
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
