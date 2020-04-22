53f9aaa4475967287e6e222c8f0662c81eb82407
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 18:46:57 +0200

    global mixins

--- modules/13/src/main.js
+++ modules/13/src/main.js
@@ -5,6 +5,12 @@ Vue.filter('toLowercase', function(value) {
     return value.toLowerCase();
 })
 
+Vue.mixin({ // global mixins are rarely used, they are called for every Vue instance or component; they often are useful only when creating plugins for Vue
+  created() {
+    console.log('Created (global mixin)');
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
