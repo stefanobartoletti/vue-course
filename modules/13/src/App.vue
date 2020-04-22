1c1eaea701b462e56709a126e5e8af5ffd798a31
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 17:26:56 +0200

    local filter

--- modules/13/src/App.vue
+++ modules/13/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{ text | toUppercase /* values are filtered with a pipe */ }}</p>
             </div>
         </div>
     </div>
@@ -11,6 +11,16 @@
 
 <script>
     export default {
+        data() {
+            return {
+                text: 'Hello there!'
+            }
+        },
+        filters: { // filters are used to modify how data is outputted/displayed, without changing it in the source
+            toUppercase(value) {
+                return value.toUpperCase();
+            }
+        }
     }
 </script>
 
