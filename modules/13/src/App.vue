ea79540c460fb79e174de6d899f8cfd5cddcae0b
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 17:58:47 +0200

    computed properties as alternative to filters

--- modules/13/src/App.vue
+++ modules/13/src/App.vue
@@ -4,6 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | toLowercase }}</p> <!-- values are filtered with a pipe -->
+                <hr>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{fruit}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -13,12 +18,21 @@
     export default {
         data() {
             return {
-                text: 'Hello there!'
+                text: 'Hello there!',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: '',
             }
         },
-        filters: { // filters are used to modify how data is outputted/displayed, without changing it in the source
+        filters: { // filters are used to modify how data is outputted/displayed, without changing it in the source. Filters are not a great choice from a performance point of view (must load the DOM to be applied), computed  properties are often a better solution
             toUppercase(value) {
                 return value.toUpperCase();
+            }
+        },
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter( (element) => {
+                    return element.match(this.filterText);
+                });
             }
         }
     }
