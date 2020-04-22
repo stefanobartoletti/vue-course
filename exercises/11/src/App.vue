a544d6902c04e5b4641e8c51c7d5853076f4dc72
stefano-b <bartoletti.stefano@gmail.com>
2020-04-22 19:39:15 +0200

    exercise 2

--- exercises/11/src/App.vue
+++ exercises/11/src/App.vue
@@ -5,12 +5,14 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
-
+                <h2>Exercise 1</h2>
                 <p>{{ name | reverseText }}</p>
 
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <h2>Exercise 2</h2>
+                <p>{{ name | stringLength }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
--- exercises/11/src/main.js
+++ exercises/11/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
+Vue.filter('stringLength', function(value) {
+    return value + " (" + value.length + ")";
 });
 
 Vue.mixin({
