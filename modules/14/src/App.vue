86cfe3a506484e60ec5260b23e11f6160adbc700
stefano-b <bartoletti.stefano@gmail.com>
2020-04-23 16:34:54 +0200

    transition tag

--- modules/14/src/App.vue
+++ modules/14/src/App.vue
@@ -3,7 +3,12 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition>
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition> 
             </div>
         </div>
     </div>
@@ -13,7 +18,7 @@
     export default {
         data() {
             return {
-
+                show: false,
             }
         }
     }
