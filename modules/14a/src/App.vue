fc3a72f4c613379d831fe269b8562464f96a2186
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 16:39:05 +0200

    basic components

--- modules/14a/src/App.vue
+++ modules/14a/src/App.vue
@@ -2,18 +2,31 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
+                <h1 class="text-center">The Super Quiz</h1>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <component :is="mode"></component>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+
     export default {
         data() {
             return {
-
+                mode: 'appQuestion',
             }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer,
         }
     }
 </script>
