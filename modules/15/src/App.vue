e4fd7635d6c782883fd81a6a8afaaf27b7e76c28
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 19:43:05 +0200

    send a POST request to a server

--- modules/15/src/App.vue
+++ modules/15/src/App.vue
@@ -29,7 +29,12 @@
         },
         methods: {
             submit() {
-                console.log(this.user);
+                this.$http.post('https://sb-vue-test.firebaseio.com/lesson15data.json', this.user)
+                    .then(response  => {
+                        console.log(response)
+                    }, error => {
+                        console.log(error)
+                    });
             }
         }
     }
