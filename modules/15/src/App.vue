c836a8e4bbcfa4e24fcbf23f58167c69efba2f0c
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 19:20:05 +0200

    app setup

--- modules/15/src/App.vue
+++ modules/15/src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,19 @@
 
 <script>
     export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: '',
+                }
+            }
+        },
+        methods: {
+            submit() {
+                console.log(this.user);
+            }
+        }
     }
 </script>
 
