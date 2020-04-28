7d1f5b3f95a93356bb5ff68f4e54809e98677777
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 17:23:52 +0200

    add animation

--- modules/14a/src/App.vue
+++ modules/14a/src/App.vue
@@ -8,7 +8,13 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                <transition name="flip" mode="out-in">
+                    <component 
+                        :is="mode"
+                        @answered="answered($event)"
+                        @confirmed="mode = 'app-question'"
+                    ></component>
+                </transition>
             </div>
         </div>
     </div>
@@ -41,3 +47,40 @@
     }
 </script>
 
+<style>
+    .flip-enter {
+        /* transform: rotateY(0deg); */
+    }
+
+    .flip-enter-active {
+        animation: flip-in 500ms ease-out forwards;
+    }
+
+    .flip-leave {
+        /* transform: rotateY(0deg); */
+    }
+
+    .flip-leave-active {
+        animation: flip-out 500ms ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
+
+
+</style>
