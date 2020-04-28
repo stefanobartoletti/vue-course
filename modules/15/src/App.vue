2542cf07824f0b9c40193a1dbe2ab71b6b895ee4
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 20:12:10 +0200

    send a GET request to a server

--- modules/15/src/App.vue
+++ modules/15/src/App.vue
@@ -12,6 +12,11 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Fetch Data</button>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,7 +29,8 @@
                 user: {
                     username: '',
                     email: '',
-                }
+                },
+                users: []
             }
         },
         methods: {
@@ -35,6 +41,19 @@
                     }, error => {
                         console.log(error)
                     });
+            },
+            fetchData() {
+                this.$http.get('https://sb-vue-test.firebaseio.com/lesson15data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data) {
+                            resultArray.push(data[key])
+                        }
+                        this.users = resultArray;
+                    })
             }
         }
     }
