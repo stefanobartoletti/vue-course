491c72c6aee1dd2582f4cfa428f43c779b9993bb
stefano-b <bartoletti.stefano@gmail.com>
2020-04-28 17:06:06 +0200

    finalized functions

--- modules/14a/src/App.vue
+++ modules/14a/src/App.vue
@@ -8,7 +8,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component :is="mode"></component>
+                <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
             </div>
         </div>
     </div>
@@ -21,7 +21,17 @@
     export default {
         data() {
             return {
-                mode: 'appQuestion',
+                mode: 'app-question',
+            }
+        },
+        methods: {
+            answered(isCorrect) {
+                if(isCorrect) {
+                    this.mode = 'app-answer';
+                } else {
+                    this.mode = 'app-question';
+                    alert('Wrong, try again')
+                }
             }
         },
         components: {
