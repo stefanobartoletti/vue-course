e5c8e380a91bcc255f989759d73c44476a48bf6d
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 19:57:59 +0200

    reacting to changes in URL parameters

--- modules/16/src/components/Header.vue
+++ modules/16/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <ul class="nav nav-pills">
         <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
-        <router-link tag="li" active-class="active" to="/user/10"><a>User</a></router-link>
+        <router-link tag="li" active-class="active" to="/user/1"><a>User 1</a></router-link>
+        <router-link tag="li" active-class="active" to="/user/2"><a>User 2</a></router-link>
     </ul>
 </template>
 
--- modules/16/src/components/user/User.vue
+++ modules/16/src/components/user/User.vue
@@ -14,6 +14,11 @@ export default {
             id: this.$route.params.id,
         }
     },
+    watch: {
+        '$route'(to, from) {
+            this.id = to.params.id;
+        }
+    },
     methods: {
         navigateToHome() {
             this.$router.push('/');
