689d1d54795098e96171daaa4e47477f8bc5dfb6
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 19:39:58 +0200

    imparative navigation ($router.push)

--- modules/16/src/components/user/User.vue
+++ modules/16/src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button @click="navigateToHome">Go Home</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
