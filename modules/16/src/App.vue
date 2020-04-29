816f0bed37ec47263fa15a52ff5427e611bd9c6a
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 18:08:14 +0200

    setting up routes

--- modules/16/src/App.vue
+++ modules/16/src/App.vue
@@ -3,6 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
+                <hr>
+                <router-view></router-view>
             </div>
         </div>
     </div>
--- modules/16/src/main.js
+++ modules/16/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes  // shortcut for "routes: routes" in ES6
+})
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
