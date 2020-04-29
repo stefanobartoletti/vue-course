664712ca5feb5100df9de40b633d23b9a91c9414
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 18:43:02 +0200

    navigation with router-link

--- modules/16/src/App.vue
+++ modules/16/src/App.vue
@@ -4,6 +4,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
+                <app-header></app-header>
                 <router-view></router-view>
             </div>
         </div>
@@ -11,7 +12,12 @@
 </template>
 
 <script>
+    import Header from './components/Header.vue';
+
     export default {
+        components: {
+            appHeader: Header,
+        }
     }
 </script>
 
---
+++ modules/16/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="nav nav-pills">
+        <li role="presentation"><router-link to="/">Home</router-link></li>
+        <li role="presentation"><router-link to="/user">User</router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
