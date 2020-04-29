34c21b6fa80d2ef5ab734556c208054b5a20b239
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 19:44:17 +0200

    setting route parameters

--- modules/16/src/components/Header.vue
+++ modules/16/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
-        <router-link tag="li" active-class="active" to="/user"><a>User</a></router-link>
+        <router-link tag="li" active-class="active" to="/user/10"><a>User</a></router-link>
     </ul>
 </template>
 
