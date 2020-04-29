beb7d928443a2881d77a8a90f32bfa960dea996c
stefano-b <bartoletti.stefano@gmail.com>
2020-04-29 19:21:37 +0200

    customizing links

--- modules/16/src/components/Header.vue
+++ modules/16/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="nav nav-pills">
-        <li role="presentation"><router-link to="/">Home</router-link></li>
-        <li role="presentation"><router-link to="/user">User</router-link></li>
+        <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
+        <router-link tag="li" active-class="active" to="/user"><a>User</a></router-link>
     </ul>
 </template>
 
