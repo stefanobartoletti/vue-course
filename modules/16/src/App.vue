e0bed10d698f44ec14bcd117115715d97ea971e1
stefano-b <bartoletti.stefano@gmail.com>
2020-04-30 20:06:12 +0200

    multiple router views (named)

--- modules/16/src/App.vue
+++ modules/16/src/App.vue
@@ -4,8 +4,10 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
-                <app-header></app-header>
+                <!-- <app-header></app-header> -->
+                <router-view name="header-top"></router-view>
                 <router-view></router-view>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
--- modules/16/src/routes.js
+++ modules/16/src/routes.js
@@ -1,13 +1,14 @@
 import Home from './components/Home.vue'
+import Header from './components/Header.vue'
 import User from './components/user/User.vue'
 import UserDetail from './components/user/UserDetail.vue'
 import UserEdit from './components/user/UserEdit.vue'
 import UserStart from './components/user/UserStart.vue'
 
 export const routes = [
-    { path: '', component: Home, name: 'home' },
+    { path: '', name: 'home', components: { default: Home, 'header-top': Header } },
     // { path: '/user/:id', component: User, props: true },
-    { path: '/user', component: User, children: [
+    { path: '/user', components: { default: User, 'header-bottom': Header }, children: [
         { path: '', component: UserStart },
         { path: ':id', component: UserDetail, props: true },
         { path: ':id/edit', component: UserEdit, name: 'userEdit', props: true },
