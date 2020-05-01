8fa5d9dacf43821a8bd21232461b4101256002f9
stefano-b <bartoletti.stefano@gmail.com>
2020-05-01 13:53:27 +0200

    loading routes lazily

--- modules/16/src/routes.js
+++ modules/16/src/routes.js
@@ -1,9 +1,30 @@
 import Home from './components/Home.vue'
 import Header from './components/Header.vue'
-import User from './components/user/User.vue'
-import UserDetail from './components/user/UserDetail.vue'
-import UserEdit from './components/user/UserEdit.vue'
-import UserStart from './components/user/UserStart.vue'
+// import User from './components/user/User.vue'
+// import UserDetail from './components/user/UserDetail.vue'
+// import UserEdit from './components/user/UserEdit.vue'
+// import UserStart from './components/user/UserStart.vue'
+
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+}
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+}
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+}
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+}
 
 export const routes = [
     { path: '', name: 'home', components: { default: Home, 'header-top': Header } },
