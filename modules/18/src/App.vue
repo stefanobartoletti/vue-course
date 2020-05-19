ee2d914b17dbf1725c9aa5a5c9d2943cfff8d3e1
stefano-b <bartoletti.stefano@gmail.com>
2020-05-19 15:26:46 +0200

    app header

--- modules/18/src/App.vue
+++ modules/18/src/App.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="container">
-        <router-view></router-view>
-    </div>
+	<div class="container">
+		<app-header></app-header>
+		<div class="row">
+			<div class="col-xs-12">
+				<router-view></router-view>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-
-    export default {
-
-    }
+import appHeader from './components/Header.vue';
+export default {
+	components: {
+		appHeader,
+	},
+};
 </script>
 
 <style>
-
+body {
+	padding: 30px;
+}
 </style>
--- modules/18/src/components/Header.vue
+++ modules/18/src/components/Header.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        <h1>Header Component</h1>
-    </div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<router-link to="/" class="navbar-brand"
+					>Stock Trader</router-link
+				>
+			</div>
+
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+					<router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">End Day</a></li>
+					<li class="dropdown">
+						<a
+							href="#"
+							class="dropdown-toggle"
+							data-toggle="dropdown"
+							role="button"
+							aria-haspopup="true"
+							aria-expanded="false"
+							>Save & Load <span class="caret"></span
+						></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Save data</a></li>
+							<li><a href="#">Load data</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
