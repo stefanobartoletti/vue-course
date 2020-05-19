d61ac0ace5e2a238cb1d89eeb01823f75adbfa11
stefano-b <bartoletti.stefano@gmail.com>
2020-05-19 16:22:26 +0200

    props and "buy" button

--- modules/18/src/components/stocks/Stock.vue
+++ modules/18/src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
 		<div class="panel panel-success">
 			<div class="panel-heading">
 				<h3 class="panel-title">
-					NAME
-					<small>Price: PRICE</small>
+					{{ stock.name }}
+					<small>(Price: {{ stock.price }})</small>
 				</h3>
 			</div>
 			<div class="panel-body">
@@ -13,18 +13,47 @@
 						type="number"
 						class="form-control"
 						placeholder="Quantity"
+						v-model.number="quantity"
 					/>
 				</div>
-                <div class="pull-right">
-                    <button class="btn btn-success">Buy</button>
-                </div>
+				<div class="pull-right">
+					<button
+						class="btn btn-success"
+						@click="buyStock"
+						:disabled="quantity <= 0 || !Number.isInteger(quantity)"
+					>
+						Buy
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			quantity: 0,
+		};
+	},
+	props: {
+		stock: {
+			type: Object,
+		},
+	},
+	methods: {
+		buyStock() {
+			const order = {
+				stockId: this.stock.id,
+				stockPrice: this.stock.price,
+				quantity: this.quantity,
+			};
+			console.log(order);
+			this.quantity = 0;
+		},
+	},
+};
 </script>
 
 <style scoped></style>
--- modules/18/src/components/stocks/Stocks.vue
+++ modules/18/src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <app-stock></app-stock>
+        <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
     </div>
 </template>
 
