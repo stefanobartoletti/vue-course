6dba6017f239336ee96b56289f9d7e21fd11e33b
stefano-b <bartoletti.stefano@gmail.com>
2020-05-19 14:54:30 +0200

    setup routes

--- modules/18/src/main.js
+++ modules/18/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
