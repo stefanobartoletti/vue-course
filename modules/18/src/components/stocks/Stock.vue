63a42364caece4222b40d4e3500e4ef6b89aa5e2
stefano-b <bartoletti.stefano@gmail.com>
2020-05-19 15:56:42 +0200

    "stocks" data and basic layout

--- modules/18/src/components/stocks/Stock.vue
+++ modules/18/src/components/stocks/Stock.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <h1>Stock (S) Component</h1>
-    </div>
+	<div class="col-sm-6 col-md-4">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					NAME
+					<small>Price: PRICE</small>
+				</h3>
+			</div>
+			<div class="panel-body">
+				<div class="pull-left">
+					<input
+						type="number"
+						class="form-control"
+						placeholder="Quantity"
+					/>
+				</div>
+                <div class="pull-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
--- modules/18/src/components/stocks/Stocks.vue
+++ modules/18/src/components/stocks/Stocks.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <h1>Stocks Component</h1>
+        <app-stock></app-stock>
     </div>
 </template>
 
 <script>
+    import appStock from './Stock.vue'
+
     export default {
-        
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'BMW', price: 110 },
+                    { id: 2, name: 'Google', price: 200 },
+                    { id: 3, name: 'Apple', price: 250 },
+                    { id: 4, name: 'Twitter', price: 8 },
+                ]
+            }
+        },
+        components: {
+            appStock,
+        },
     }
 </script>
 
