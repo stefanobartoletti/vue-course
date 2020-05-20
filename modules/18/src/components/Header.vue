9674179aa1915785437a4dc7bc5fcad17533ad00
stefano-b <bartoletti.stefano@gmail.com>
2020-05-20 17:38:25 +0200

    dropdown functionality

--- modules/18/src/components/Header.vue
+++ modules/18/src/components/Header.vue
@@ -13,7 +13,7 @@
 				<strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="#" @click="endDay">End Day</a></li>
-					<li class="dropdown">
+					<li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
 						<a
 							href="#"
 							class="dropdown-toggle"
@@ -40,6 +40,11 @@
 import {mapActions} from 'vuex';
 
 export default {
+	data() {
+		return {
+			isDropdownOpen: false,
+		}
+	},
 	computed: {
 		funds() {
 			return this.$store.getters.funds;
