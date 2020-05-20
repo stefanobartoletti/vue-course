1c878b75d2dc14987de9bbd9af1553249a381978
stefano-b <bartoletti.stefano@gmail.com>
2020-05-20 17:29:39 +0200

    animation

--- modules/18/src/App.vue
+++ modules/18/src/App.vue
@@ -3,7 +3,9 @@
 		<app-header></app-header>
 		<div class="row">
 			<div class="col-xs-12">
-				<router-view></router-view>
+				<transition name="slide" mode="out-in">
+					<router-view></router-view>
+				</transition>
 			</div>
 		</div>
 	</div>
@@ -17,7 +19,7 @@ export default {
 	},
 	created() {
 		this.$store.dispatch('initStocks');
-	}
+	},
 };
 </script>
 
@@ -25,4 +27,33 @@ export default {
 body {
 	padding: 30px;
 }
+
+.slide-enter-active {
+	animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+	animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+	from {
+		transform: translateY(-30px);
+		opacity: 0;
+	} to {
+		transform: translateY(0);
+		opacity: 1;
+	}
+}
+
+@keyframes slide-out {
+	from {
+		transform: translateY(0px);
+		opacity: 1;
+	} to {
+		transform: translateY(-30px);
+		opacity: 0;
+	}
+}
+
 </style>
