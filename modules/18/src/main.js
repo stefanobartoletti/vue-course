8f13dec073ad2aca36c33ea2b2e9cc21cace8c71
stefano-b <bartoletti.stefano@gmail.com>
2020-05-20 17:53:19 +0200

    setup vue-resource

--- modules/18/src/main.js
+++ modules/18/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import App from './App.vue'
 import { routes } from './routes'
 import store from './store/store'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://sb-vue-stock.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '$' + value .toLocaleString();
