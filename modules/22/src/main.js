9cfb543ea861325a8a42470e3575f066a7bc13eb
stefano-b <bartoletti.stefano@gmail.com>
2020-05-21 17:04:36 +0200

    axios interceptors

--- modules/22/src/main.js
+++ modules/22/src/main.js
@@ -9,6 +9,15 @@ axios.defaults.baseURL = 'https://sb-vue-axios.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'qwerty';
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+axios.interceptors.request.use(config => {
+  console.log('Request interceptor', config)
+  return config
+})
+axios.interceptors.response.use(res => {
+  console.log('Response interceptor', res)
+  return res
+})
+
 new Vue({
   el: '#app',
   router,
