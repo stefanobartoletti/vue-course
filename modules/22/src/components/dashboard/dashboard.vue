7c892d464fbf87ab07d8bf2cbe124d515ec6ef85
stefano-b <bartoletti.stefano@gmail.com>
2020-05-21 15:56:43 +0200

    GET request

--- modules/22/src/components/dashboard/dashboard.vue
+++ modules/22/src/components/dashboard/dashboard.vue
@@ -1,16 +1,29 @@
 <template>
-  <div id="dashboard">
-    <h1>That's the dashboard!</h1>
-    <p>You should only get here if you're authenticated!</p>
-  </div>
+	<div id="dashboard">
+		<h1>That's the dashboard!</h1>
+		<p>You should only get here if you're authenticated!</p>
+	</div>
 </template>
 
-<style scoped>
-  h1, p {
-    text-align: center;
-  }
+<script>
+import axios from 'axios';
 
-  p {
-    color: red;
-  }
+export default {
+	created() {
+         axios.get('https://sb-vue-axios.firebaseio.com/users.json')
+         .then(response => console.log(response))
+         .catch(error => console.log(error))
+    },
+};
+</script>
+
+<style scoped>
+h1,
+p {
+	text-align: center;
+}
+
+p {
+	color: red;
+}
 </style>
